5d76ae6259b729be35f34539566085e675430380
Daivashala Dhepe <daivapatil7@gmail.com>
2022-10-04 08:36:09 +0530

    added demo.js
    added demo.js

---
+++ demo.js
@@ -0,0 +1,8 @@
+console.log("Hello");
+// this is my first program 
+console.log("My name is Daivashala ");
+// info means information 
+  console.info(" This is info "); 
+//   warn means warming 
+  console.warn(" I am not that bad guy");
+  console.error(" Opps failed to execute ")
