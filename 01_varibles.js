15a5e2f928a85640a9f9ff78419dcc56c35fb5e4
Daivashala Dhepe <daivapatil7@gmail.com>
2022-10-06 14:18:41 +0530

    variables and swap_variables .JS
    variables and swap_variables .JS

---
+++ 01_varibles.js
@@ -0,0 +1,12 @@
+
+   
+var my_apple;  //  variable declaration
+
+my_apple ="Strawberry";  //   variable initialization  
+  
+
+//   var my_name ="Daiva Patil"; //   variable declaration and initialization
+//my_name ="Daivashala Gangadhar Dhepe";
+my_apple ="200"; 
+console.log(my_name);
+console.log(my_apple);
