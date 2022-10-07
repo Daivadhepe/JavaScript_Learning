50bbdb6e5df119bfee37d71b4fea9915e56a2523
Daivashala Dhepe <daivapatil7@gmail.com>
2022-10-07 09:16:08 +0530

    add function point
    add function point

---
+++ 02_daratypes.js
@@ -0,0 +1,60 @@
+
+
+
+var d_name="Daivashala";
+var d_age= 23;
+var d_city="Pune";
+var married_status=true;
+
+var value_type;
+value_type= typeof "Daiva";
+console.log(value_type);
+console.log(typeof 31);
+console.log(typeof Daiva);
+console.log(typeof false);
+console.log("=========");
+
+console.log(typeof p_name);
+console.log(typeof age);
+console.log(typeof married_status);
+
+console.log("====================================");
+var my_project_name ;
+console.log(my_project_name);
+console.log("===============");
+console.log(typeof my_project_name);
+console.log("=========================");
+var my_properties = null;
+console.log(typeof my_properties);
+
+console.log("=====================================================================");
+var p_name = "Prashant";
+var age = 22;
+var city = 'Pune';
+var married_status = false;
+
+console.log(typeof 31 );
+console.log(typeof false);
+console.log(typeof "Pradnya");
+console.log("================");
+console.log(typeof p_name);
+console.log(typeof age);
+console.log(typeof married_status);
+
+console.log("================");
+var my_project_name;
+console.log(my_project_name);
+console.log("================");
+console.log(typeof my_project_name);
+
+var my_properties = null;
+
+console.log(typeof my_properties);
+
+
+
+
+
+
+
+
---
+++ 03_function.js
@@ -0,0 +1,66 @@
+
+
+ function sachin(){
+    console.log("Sachin is very good boy");
+    console.log("He has good sense");
+
+}
+sachin();
+
+function daiva(money){
+    console.log(money);
+    console.log("please go in market with Rs.",money);
+    console.log("buy some vegetables");
+    return "Vegetables bag";
+}
+   var veg = daiva(500);
+   console.log(veg);
+
+   function functionName(city,street,age,is_married ){
+     console.log(city , street , age , is_married);
+    return city+ street+ age+ is_married; 
+
+   }
+   functionName("Pune","khawgalli",22);
+   console.log(value);
+
+function swap_value(arg1,arg2){
+console.log("-------Before swap------");
+console.log(arg1,arg2);
+console.log("--------After swap-----");
+var temp = arg1;
+aeg1=arg2;
+arg2=temp;
+console.log(arg1,arg2);
+
+}
+swap_value("Virat","Anushka");
+swap_value( 100,300);
+swap_value("Mango","Apple");
+console.log(",,,,,,,,,,,addition function,,,,,,,,,,,,,,,");
+function addition(arg1,arg2,arg3){
+console.log(arg1 + arg2 + arg3);
+}
+addition(200,300,600);
+addition("sachin","ramesh","tendulkar");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
