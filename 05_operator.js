1a90877c6dafd4dddc27927d24e3aac1cd2180a5
Daivashala Dhepe <daivapatil7@gmail.com>
2022-10-12 19:52:55 +0530

    add operator and ternary operator filee
    add operator and ternary operator file

---
+++ 05_operator.js
@@ -0,0 +1,65 @@
+console.log(10+20);
+console.log(10+10.23);
+
+var num1 = 200;
+var num2 = 400;
+var result = num1 + num2;
+console.log(`Result is : ${result}`);
+
+console.log("================================================================");
+
+var num1=300;
+var num2=100;
+var result= num1-num2;
+console.log(`result is=${result}`);
+
+console.log("============module operator/ remainder===========================");
+var num1=50;
+var num2=2;
+var result = num1%num2;
+console.log(`result is = ${result}`);
+console.log("this no is even no.");
+console.log("=============== ++Increment===============");
+var num1=30
+var num2=4
+// var incr = num1+1;
+var incr = ++num1 ;
+ var decr = --num2;
+console.log(`result is = ${incr} ${decr} `);
+console.log("=============compound Assignment===========================");
+var num1 = 100;
+var num2 = 50; 
+num1+=num2;// num1 = num1 + num2;
+console.log(`Compound Addition operator : += ====> ${num1}`);
+console.log("=============compound Assignment===========================");
+var num1 = 100;
+var num2 = 50; 
+num1-=num2;
+console.log(`compound substitution operator = -= ====> ${num1}`);
+console.log("============================comparison operator============================");
+
+var num1 = 100;
+var num2 = 50; 
+
+console.log("num1<num2 ===> ",   num1<num2);
+console.log("num1>num2 ===> ",  num1>num2);
+console.log("num1!=num2 ====> ", num1!=num2);
+
+var num3 = 50; 
+console.log("num2<num3) ===> ",  num2<num3);
+console.log("num2<=num3) ===> ",  num2<=num3);
+console.log("num2>num3=====> ", num2>num3);
+console.log("num2>=num3=====> ", num2>=num3);
+
+console.log(`******* == operator ************`);
+var num2 = 50; 
+var num3 = "50"; 
+var result = num2 == num3; // 50 == 50
+
+console.log("num2==num3=====> ", result);
+
+console.log("num2===num3=====> ", num2===num3);
+
+
+
+
--- parshuramAssignment.js
+++ parshuramAssignment.js
@@ -31,5 +31,80 @@ var hobby_3 = "Cooking"
 console.log(`Hobby 1 : ${hobby_1}`);
 console.log(`Hobby 2 : ${hobby_2}`);
 console.log(`Hobby 3 : ${hobby_3}`);
+
+
+
 console.log(`After concat : ${hobby_1.concat(" ", hobby_2, " ", hobby_3)}`);
-``
+
+
+
+console.log("==========================================operator ==============================================================");
+var wordLengthSquare = function (square) {
+    console.log(`Given word : ${square}`);
+    console.log(`length of word : ${square.length}`);
+    var wordlength = square.length;
+    return wordlength*wordlength;
+}
+console.log(`Square of word : ${wordLengthSquare("JavaScript")}`);
+console.log('==========================================');
+console.log(`Square of word : ${wordLengthSquare("Google")}`);
+console.log('==========================================');
+console.log(`Square of word : ${wordLengthSquare("Developer")}`);
+console.log('==================== 2) Done with the help of Google======================');
+var stringLine = function () {
+    stringLine= "I am Angular Developer";
+    console.log(stringLine);
+    var splitstring = stringLine.split(" ")
+    console.log(splitstring);
+    var reversearray = splitstring.reverse();   
+    console.log(reversearray);
+    var joinarray = reversearray.join(" ")
+    console.log(joinarray);
+    console.log('==========================================');
+   var stringlength = stringLine.length
+   console.log(`string total length : ${stringlength}`);
+   var totalwords = stringLine.split(" ")
+   var totalwordslength = totalwords.length
+   console.log(`Total no. of words : ${totalwordslength}`);
+   console.log(`stringlength Divided by total no. of word : ${stringlength/totalwordslength}`);
+   console.log(`stringlength multiply by total no. of word : ${stringlength*totalwordslength}`);
+}
+
+console.log("===============================================ternary operator========================================================");
+var greatestNo = function (num1, num2) {
+    var result = num1 > num2 ? num1 : num2;
+    console.log(`Greater number is : ${result}`);
+
+}
+greatestNo(10, -10);
+greatestNo(800, 899)
+console.log("=====================2=========================");
+var evenOddValue = function (num) {
+    var result = num % 2 == 0 ? true : false;
+    return result;
+
+}
+var returnvalue = evenOddValue(29);
+console.log(`is value true or false: ${returnvalue}`);
+var returnvalue2 = evenOddValue(44);
+console.log(`is value true or false: ${returnvalue2}`);
+var returnvalue3 = evenOddValue(0);
+console.log(`is value true or false : ${returnvalue3}`);
+var returnvalue4 = evenOddValue(101);
+console.log(`is value true or false: ${returnvalue4}`);
+console.log("=====================3=========================");
+var evenOddLength = function (word) {
+    var resultwordlength = word.length;
+    console.log(`word length is : ${resultwordlength}`);
+    var iswordEvenOdd = resultwordlength % 2 == 0 ? "EVEN" : "ODD";
+    return iswordEvenOdd;
+}
+var evenOrOdd = evenOddLength("Javascript");
+console.log(`is word is even or odd : ${evenOrOdd}`);
+var evenOrOdd2 = evenOddLength("developer");
+console.log(`is word is even or odd : ${evenOrOdd2}`);
+var evenOrOdd3 = evenOddLength("Google");
+console.log(`is word is even or odd : ${evenOrOdd3}`);
+
+
+
